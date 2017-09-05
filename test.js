d085daf8b9fe25a6e0450ee6efd4f9baab5960f3
Millie Walsh <millie@primeacademy.io>
2017-09-05 11:45:01 -0500

    update lecture .data example with comments

--- test.js
+++ test.js
@@ -19,8 +19,8 @@ function onReady() {
 // called when the delete button is clicked
 // logs which can is being removed
 function removeCan() {
-    console.log('removing can number ', $(this).parent().data('color'));
-    // console.log($(this).parent());
+    // retrieve the can count info from the parent article and log it.
+    console.log('removing can number ', $(this).parent().data('counter'));
     $(this).parent().remove();
 }
 
@@ -35,7 +35,8 @@ function addCan() {
     canCounter++; // increment canCounter
     console.log('called addCan', canCounter);
     
-    var $canArticle = $('<article>').data('color', canCounter);
+    // add data-counter to the can article to keep track of which can this is.
+    var $canArticle = $('<article>').data('counter', canCounter);
     
     $canArticle.append($('<h3>', {text: 'SPAM'}));
     $canArticle.append($('<img />', {class: 'spam-img', src:'https://upload.wikimedia.org/wikipedia/commons/0/09/Spam_can.png'}));
